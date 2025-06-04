0b56c679c95b2caf776faaee0cf184ee01f9df37
rudolfvegh <tankyboy@gmail.com>
2025-06-04 20:27:15 +0200

    movement added

--- main.py
+++ main.py
@@ -20,7 +20,9 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, (0,0,0))
+        
+        player.update(dt)
+        screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
         # limit the framerate to 60 FPS
